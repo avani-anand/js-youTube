57a86da950b4a643c09de3e75c1d28a3d49d3543
Avani Anand <iamavanianand@gmail.com>
2024-01-06 12:06:48 +0530

    "learned difference bwtween const and var"

---
+++ 01_basics/variable.js
@@ -0,0 +1,24 @@
+const accountId= 12343;
+let accountEmail="hitesh@google.com"
+var accountPassword="12345"
+accountCity ="jaipur"
+
+
+
+// accountId=123
+
+accountEmail="hit@gmil"
+accountPassword="1232424"
+accountCity ="bengaluru"
+
+console.table([accountEmail,accountId,accountPassword])
+
+// output ----------------------
+
+// ┌─────────┬────────────┐
+// │ (index) │   Values   │
+// ├─────────┼────────────┤
+// │    0    │ 'hit@gmil' │
+// │    1    │   12343    │
+// │    2    │ '1232424'  │
+// └─────────┴────────────┘
